e469f825b99f1023517c3dc2c7896341fc6b9080
Daniel Robertson <danlrobertson89@gmail.com>
2015-12-12 21:16:40 -0500

    Add db authentication
    Web app should run in read-only mode. There is no need to write to the
    db. The user starting the app should have the ability to choose the db
    user to run the app under and challenge the user with the appropriate
    password.
    
    Signed-off-by: Daniel Robertson <danlrobertson89@gmail.com>

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-var express        = require("express")
+var express      = require("express")
+  , read         = require("read")
   , MongoClient  = require("mongodb").MongoClient
   , cons         = require("consolidate")
   , routes       = require("./routes")
@@ -10,23 +11,48 @@ var express        = require("express")
   , path         = require("path")
   , app          = express();
 
-MongoClient.connect("mongodb://localhost:27017/crawler", function(err, db) {
-    "use strict";
-    if(err) throw err;
+user_input = {
+    "usr": "",
+};
 
-    app.engine("html", cons.swig);
+var i = 2;
+var argv = process.argv;
+var new_len = argv.length - 1;
+while(i < new_len) {
+    var arg = argv[i].replace("--", "");
+    if(arg in user_input) {
+        user_input[arg] = argv[i + 1];
+    } else {
+        throw Error("Could not parse user input: " + argv[i]);
+    }
+    i += 2;
+}
 
-    app.set("view engine", "html");
 
-    app.use(express.static(path.join(__dirname, 'public')));
+read({"prompt": "Password: ", "silent": true}, function(err, pwd) {
+    MongoClient.connect("mongodb://localhost:27017/crawler", function(err, db) {
+        db.authenticate(user_input["usr"], pwd, function(err, result) {
+            "use strict";
 
-    app.set("views", __dirname + "/views");
+            if(!result) throw Error("Failed to log in with user - " + user_input["usr"])
 
-    app.use(cookieParser());
+            if(err) throw err;
 
-    routes(app, db);
-
-    app.listen(3000);
-
-    console.log("Express server listening on port 3000");
+            app.engine("html", cons.swig);
+    
+            app.set("view engine", "html");
+    
+            app.use(express.static(path.join(__dirname, 'public')));
+    
+            app.set("views", __dirname + "/views");
+    
+            app.use(cookieParser());
+    
+            routes(app, db);
+    
+            app.listen(3000);
+    
+            console.log("Express server listening on port 3000");
+        });
+    });
 });
